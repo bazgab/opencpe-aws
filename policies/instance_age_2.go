b01421163923ceb2ce7ac09b0f27478b1ba97b79
Gabriel Bazzotti <gabriel.bazzotti@suse.com>
2025-12-02 09:32:17 -0300

    chore(policies): cleanup for rebase

--- policies/instance_age_2.go
+++ policies/instance_age_2.go
@@ -7,7 +7,8 @@ import (
 
 func InstanceAge2Days(profile string, region string) {
 	config.LoadConfig(profile, region)
-	fmt.Println("This is from the policies package")
+	fmt.Println("Policy: Instance-age-2-Days")
+	fmt.Println("-- Description: Checks for EC2 instances that are over 2 days old")
 	fmt.Printf("Profile: %s\n", profile)
 	fmt.Printf("Region: %s\n", region)
 }
